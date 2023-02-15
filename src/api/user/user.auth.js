b13ff124d0226d69230821b832070ea0670bf6fc
Anjali Gurjar <anjaligurjar861@gmail.com>
2023-02-15 11:17:51 +0530

    this is auth

--- src/api/user/user.auth.js
+++ src/api/user/user.auth.js
@@ -1,4 +1,35 @@
 const jwt = require('jsonwebtoken')
 exports.verifyToken=(req,res,next)=>{
+    const auth =req.auth
+    console.log(auth)
+    if(!auth.scheme==bearer){
+       return  res.send("invalie shceme")
+    }
+    jwt.verifyToken( auth.token, SECRETE,(error,data)=>{
+if(error){
+    return res.send("access is not  verify ")
+    
+}
+req.auth.user=data.user
+
+    })
+exports.verify(user,cb)=
+    jwt.sign({user},SECRETE,({expireIn:'300s'},()=>{
+        
+
+    }))
+exports.extract=( res,res,next)=>{
+    const bear=req.Authurization['']
+        const auth=bear.split(" ")
+    req.auth={
+        scheme:auth[0],
+        token:auth[1]
+        
+    }
+    if(!req.auth.token){
+        res.send('access denied')
+    }
+    }
 
 }
+
